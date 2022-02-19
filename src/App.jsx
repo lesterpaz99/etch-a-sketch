4a8288080f615c1545900eb97d2a051be5db0466
Obed Paz <lesterpaz99@gmail.com>
2022-02-19 00:12:22 -0600

    update the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-import './App.css';
+import { SideBar } from './components/SideBar';
+import { Screen } from './components/Screen';
+import styles from './App.module.scss';
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className={styles.App}>
+			<SideBar />
+			<main>
+				<h1>Etch-a-sketch / Sketchpad</h1>
+				<Screen />
+			</main>
+		</div>
+	);
 }
 
 export default App;
