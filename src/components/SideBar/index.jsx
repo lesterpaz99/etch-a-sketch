af5cc4729bb4d70e2aca05f539f54a4f73405e15
Obed Paz <lesterpaz99@gmail.com>
2022-02-19 23:12:34 -0600

    consume the context and add utils functions

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useRef, useContext } from 'react';
+import { globalColor } from '../../context/globalColor';
 import { IconContext } from 'react-icons';
 import { CgMenuMotion } from 'react-icons/cg';
 import { IoColorPalette } from 'react-icons/io5';
@@ -8,13 +9,22 @@ import styles from './SideBar.module.scss';
 
 const SideBar = () => {
 	const [open, setOpen] = useState(false);
+	const { color, mode, handleColor, handleMode } = useContext(globalColor);
+	const colorValue = useRef(null);
 
 	const handleOpenAside = () => {
 		setTimeout(() => setOpen(true), 400);
 	};
+
 	const handleCloseAside = () => {
 		setOpen(false);
 	};
+
+	const handleInputColor = () => {
+		handleColor(colorValue.current.value);
+	};
+
+	const focusColor = 'rgba(128, 128, 128, 0.2)';
 
 	return (
 		<aside
@@ -30,18 +40,26 @@ const SideBar = () => {
 			<div className={styles.optionsContainer}>
 				<IconContext.Provider value={{ className: styles.icons }}>
 					<div className={styles.changeColor}>
-						<button type='button'>
+						<button type='button' onClick={() => handleMode('color')}>
 							{<IoColorPalette />} {open && <span>Color mode</span>}
 						</button>
-						{open && <input type='color' className={styles.inputColor} />}
+						{open && (
+							<input
+								type='color'
+								className={styles.inputColor}
+								ref={colorValue}
+								value={color}
+								onChange={handleInputColor}
+							/>
+						)}
 					</div>
-					<button type='button'>
+					<button type='button' onClick={() => handleMode('rainbow')}>
 						{<FaRainbow />} {open && <span>Rainbow mode</span>}
 					</button>
-					<button type='button'>
+					<button type='button' onClick={() => handleMode('eraser')}>
 						{<FaEraser />} {open && <span>Eraser</span>}
 					</button>
-					<button type='button'>
+					<button type='button' onClick={() => handleMode('clean')}>
 						{<AiOutlineClear />} {open && <span>Clear</span>}
 					</button>
 				</IconContext.Provider>
