d4298b2e53fadf8b3399a62d83d0ab20ae9af4ec
Obed Paz <lesterpaz99@gmail.com>
2022-02-19 23:04:56 -0600

    provide the context to the entire App

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { SideBar } from './components/SideBar';
 import { Screen } from './components/Screen';
+import { GlobalColorProvider } from './context/globalColor';
 import styles from './App.module.scss';
 
 function App() {
 	return (
-		<div className={styles.App}>
-			<SideBar />
-			<main>
-				<h1>Etch-a-sketch / Sketchpad</h1>
-				<Screen />
-			</main>
-		</div>
+		<GlobalColorProvider>
+			<div className={styles.App}>
+				<SideBar />
+				<main>
+					<h1>Etch-a-sketch / Sketchpad</h1>
+					<Screen />
+				</main>
+			</div>
+		</GlobalColorProvider>
 	);
 }
 
