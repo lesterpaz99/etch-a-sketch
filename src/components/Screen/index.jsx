4784472f90fc2e4e3804e1424cb27df965b2cf0b
Obed Paz <lesterpaz99@gmail.com>
2022-02-19 23:09:34 -0600

    add functions to work with the grid

--- src/components/Screen/index.jsx
+++ src/components/Screen/index.jsx
@@ -1,11 +1,22 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useContext } from 'react';
+import { globalColor } from '../../context/globalColor';
 import styles from './Screen.module.scss';
 
 const Screen = () => {
-	const [size, setSize] = useState(16);
+	const [size, setSize] = useState(64);
+	const { color, mode, handleMode } = useContext(globalColor);
 	const inputRange = useRef(null);
+	const gridContainer = useRef(null);
 
-	const handleInputValue = () => setSize(inputRange.current.value);
+	const clean = () => {
+		const gridChildren = Array.from(gridContainer.current.children);
+		gridChildren.forEach((child) => (child.style.backgroundColor = '#ffffff'));
+	};
+
+	const handleInputValue = () => {
+		setSize(inputRange.current.value);
+		clean();
+	};
 
 	const arr = new Array(size * size).fill('div');
 
@@ -15,31 +26,62 @@ const Screen = () => {
 		gridTemplateRows: `repeat(${size}, 1fr)`,
 	};
 
-	const handleColor = (e) => {
-		e.target.style.backgroundColor = 'black';
+	const setRandomColor = () => {
+		const randomColor1 = Math.floor(Math.random() * 256);
+		const randomColor2 = Math.floor(Math.random() * 256);
+		const randomColor3 = Math.floor(Math.random() * 256);
+
+		return `rgb(${randomColor1}, ${randomColor2}, ${randomColor3})`;
 	};
 
+	const handleColor = (index) => {
+		const target = gridContainer.current.children[index];
+		if (mode === 'color') {
+			target.style.backgroundColor = color;
+		}
+		if (mode === 'eraser') {
+			target.style.backgroundColor = '#ffffff';
+		}
+		if (mode === 'rainbow') {
+			target.style.backgroundColor = setRandomColor();
+		}
+	};
+
+	if (mode === 'clean') {
+		clean();
+		handleMode('color');
+	}
+
 	return (
-		<div className=''>
-			<label>
-				Pixel size: {size}
-				<input
-					type='range'
-					value={size}
-					min={1}
-					max={64}
-					ref={inputRange}
-					onChange={handleInputValue}
-				/>
+		<div>
+			<label className={styles.labelRangeWrapper}>
+				<p>Adjust:</p>
+				<div className={styles.iRangeContainer}>
+					<span>-</span>{' '}
+					<input
+						type='range'
+						value={size}
+						min={16}
+						step={16}
+						max={64}
+						ref={inputRange}
+						onChange={handleInputValue}
+					/>{' '}
+					<span>+</span>
+				</div>
 			</label>
 			<div
 				style={gridSize}
 				className={styles.screenContainer}
-				onMouseOver={handleColor}
-				onMouseDown={handleColor}
+				ref={gridContainer}
 			>
 				{arr.map((divItem, index) => (
-					<div className={styles.divPixel} key={divItem + index}></div>
+					<div
+						className={styles.divPixel}
+						key={divItem + index}
+						name={divItem + index}
+						onMouseOver={() => handleColor(index)}
+					></div>
 				))}
 			</div>
 		</div>
